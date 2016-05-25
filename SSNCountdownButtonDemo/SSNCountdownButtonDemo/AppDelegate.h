114984caf6bbef06e5441ffac5970a81842a22eb
sosoneo <sosoneo@163.com>
2016-05-25 15:30:54 +0800

    open

---
+++ SSNCountdownButtonDemo/SSNCountdownButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSNCountdownButtonDemo
+//
+//  Created by zhonzhon_iOS on 16/5/25.
+//  Copyright © 2016年 zhonzhon_iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSNCountdownButtonDemo/SSNCountdownButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSNCountdownButtonDemo
+//
+//  Created by zhonzhon_iOS on 16/5/25.
+//  Copyright © 2016年 zhonzhon_iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
